9279cd59289db2354aa9bdb1c7736af34197c827
realbiplobhossen-lab <real.biplob.hossen@gmail.com>
2026-06-05 14:59:38 +0600

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,31 +1,19 @@
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
-import {
-  getAuth
-} from "firebase/auth";
-
-import {
-  getFirestore
-} from "firebase/firestore";
-
+// আপনার নিজস্ব Firebase Configuration কনফিগারেশন (গিটহাবে যা দেওয়া আছে)
 const firebaseConfig = {
-
-  apiKey: "YOUR_API_KEY",
-
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-
-  projectId: "YOUR_PROJECT_ID",
-
-  storageBucket: "YOUR_PROJECT.appspot.com",
-
-  messagingSenderId: "YOUR_SENDER_ID",
-
-  appId: "YOUR_APP_ID"
-
+  apiKey: "YOUR_API_KEY_HERE",
+  authDomain: "YOUR_AUTH_DOMAIN_HERE",
+  projectId: "YOUR_PROJECT_ID_HERE",
+  storageBucket: "YOUR_STORAGE_BUCKET_HERE",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID_HERE",
+  appId: "YOUR_APP_ID_HERE"
 };
 
+// ইনিশিয়ালাইজেশন
 const app = initializeApp(firebaseConfig);
 
+// auth-কে এক্সপোর্ট করা হলো যা App.jsx সরাসরি রুট থেকে রিড করবে
 export const auth = getAuth(app);
-
-export const db = getFirestore(app);
+export default app;
