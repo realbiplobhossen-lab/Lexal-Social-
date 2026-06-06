fd0e9e7dfecbcce105778ad61416c409293d8ca1
realbiplobhossen-lab <real.biplob.hossen@gmail.com>
2026-06-06 11:49:50 +0600

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore"; // 👈 ফায়ারস্টোর ইমপোর্ট করা হলো
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAG4341Rk7sr1IJkLfVdbDT-oyuCB_GwjA",
@@ -11,11 +11,7 @@ const firebaseConfig = {
   appId: "1:251651994141:web:9356eb4895de76b7b30cf4"
 };
 
-// ফায়ারবেস অ্যাপ ইনিশিয়ালাইজ করা হলো
 const app = initializeApp(firebaseConfig);
-
-// অথেনটিকেশন এবং ফায়ারস্টোর ডাটাবেজ অবজেক্ট তৈরি ও এক্সপোর্ট
 export const auth = getAuth(app);
-export const db = getFirestore(app); // 👈 এই লাইনটিই গিটহাব খুঁজছে এবং এর ফিক্স
-
+export const db = getFirestore(app);
 export default app;
