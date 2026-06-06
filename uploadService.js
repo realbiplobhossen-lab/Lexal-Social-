e3b8b4fbd0ec26e3c7d6637c4dfeb5d3da06f356
realbiplobhossen-lab <real.biplob.hossen@gmail.com>
2026-06-07 03:39:46 +0600

    Update uploadService.js

--- uploadService.js
+++ uploadService.js
@@ -1,35 +1,22 @@
-import axios from "axios";
+import { cloudinaryConfig } from '../config/cloudinary';
 
-import {
-CLOUD_NAME,
-UPLOAD_PRESET
-}
-from "../config/cloudinary";
-
-export async function uploadImage(file){
-
-const formData =
-new FormData();
-
-formData.append(
-"file",
-file
-);
-
-formData.append(
-"upload_preset",
-UPLOAD_PRESET
-);
-
-const response =
-await axios.post(
-
-`https://api.cloudinary.com/v1_1/${CLOUD_NAME}/image/upload`,
-
-formData
-
-);
-
-return response.data.secure_url;
-
-}
+export const uploadService = {
+  uploadMedia: async (file) => {
+    if (!file) return { url: null, type: 'text' };
+    
+    const formData = new FormData();
+    formData.append("file", file);
+    formData.append("upload_preset", cloudinaryConfig.uploadPreset);
+    
+    const response = await fetch(`https://api.cloudinary.com/v1_1/${cloudinaryConfig.cloudName}/auto/upload`, {
+      method: "POST",
+      body: formData
+    });
+    
+    const data = await response.json();
+    return {
+      url: data.secure_url,
+      type: data.resource_type // 'image' or 'video'
+    };
+  }
+};
