adaee0fed5d5b9ef2edfefddcff71c8748da1ec7
realbiplobhossen-lab <real.biplob.hossen@gmail.com>
2026-06-07 03:15:00 +0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './' // এটি অত্যন্ত জরুরি যাতে মোবাইলে ফাইল পাথ মিস না হয়
+  server: {
+    port: 3000
+  },
+  build: {
+    outDir: 'dist'
+  }
 })
-
